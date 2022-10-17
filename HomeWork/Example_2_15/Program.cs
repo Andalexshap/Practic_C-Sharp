5a36243e79cb92231040528741db80352332e412
Andalexshap <andalexshap@yandex.ru>
2022-10-17 13:26:39 +0300

    Add Example 2_15

---
+++ HomeWork/Example_2_15/Program.cs
@@ -0,0 +1,48 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.WriteLine("Введите цифру от 1 - 7, соответсвующую дню недели, либо название дня недели");
+
+string[] dayOfWeek = {"понедельник","вторник","среда","четверг","пятница","суббота","воскресенье"};
+
+int number = 1;
+string value = Console.ReadLine();
+bool result = int.TryParse(value, out number);
+
+
+
+if(!result)
+{
+    string str = value.ToLower();
+
+    if (str == dayOfWeek[0] || str == dayOfWeek[1] ||str == dayOfWeek[2] ||str == dayOfWeek[3] ||str == dayOfWeek[4])
+    {
+        Console.WriteLine("Нет. Рабочий день!");
+    }
+    else if (str == dayOfWeek[5] || str == dayOfWeek[6])
+    {
+        Console.WriteLine("Да. Выходной!");
+    }
+    else
+    {
+        Console.WriteLine("Такого дня недели нет!");
+    }
+}
+else
+{
+    if (number == 6 || number == 7)
+    {
+        Console.WriteLine("Да.Выходной!");
+    }
+    else if(number < 1 || number > 7) 
+    {
+        Console.WriteLine("Число не соответсвует дню недели!");
+    }
+    else
+    {
+        Console.WriteLine("Нет. Рабочий день!");
+    }
+}
