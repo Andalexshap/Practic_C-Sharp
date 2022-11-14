21b0f885fd4913a58d09c1e7690b5d91ac8f8bca
Andalexshap <andalexshap@yandex.ru>
2022-11-14 19:00:34 +0300

    HW 6

---
+++ HomeWork/Example_6_41/Program.cs
@@ -0,0 +1,85 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.WriteLine("Программа покажет сколько чисел введенных Вами являются положительными");
+
+while (true)
+{
+    Console.WriteLine("Введите ряд чисел разделенных пробелом:");
+
+    string numbers = Console.ReadLine()!;
+
+    if (string.IsNullOrEmpty(numbers))
+    {
+        Console.WriteLine("Вы не ввели ни одного числа! Давайте сначала.");
+        continue;
+    }
+
+    string[] stringNumbersArray = ConvertStringNumbersToArraySubstring(numbers);
+    int[] intNumbersArray = new int[stringNumbersArray.Length];
+
+    try
+    {
+        intNumbersArray = ConvertArrayStringToArrayInt(stringNumbersArray);
+    }
+    catch
+    {
+        Console.WriteLine("Проверьте правильность ввода и повторите попытку");
+        continue;
+    }
+
+    int[] positiveNumbers = PositivNumbers(intNumbersArray);
+    PrintResult(positiveNumbers);
+
+    break;
+}
+
+#region Methods
+string[] ConvertStringNumbersToArraySubstring(string numbers)
+{
+    string[] stringNumbersArray = numbers.Split(' ');
+    return stringNumbersArray;
+}
+
+int[] ConvertArrayStringToArrayInt(string[] numbers)
+{
+    int[] intNumbersArray = new int[numbers.Length];
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        intNumbersArray[i] = Int32.Parse(numbers[i]);
+    }
+    return intNumbersArray;
+}
+
+int[] PositivNumbers(int[] numbers)
+{
+    List<int> PositivNumbers = new List<int>();
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0) PositivNumbers.Add(numbers[i]);
+    }
+
+    return PositivNumbers.ToArray();
+}
+
+void PrintResult(int[] numbers)
+{
+    if (numbers.Length != 0)
+    {
+        Console.WriteLine($"Колличество положительных чисел: {numbers.Length}");
+        Console.Write("Положительные числа: ");
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write($"{numbers[i]} ");
+        }
+    }
+    else Console.WriteLine($"Колличество положительных чисел: {numbers.Length}");
+}
+
+#endregion
---
+++ HomeWork/Example_6_43/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+Console.WriteLine("Программа найдёт точку пересечения двух прямых, заданных уравнениями \ny = k1 * x + b1, \ny = k2 * x + b2;");
+Console.WriteLine("Вам необходимо задать числа k1, b1, k2, b2");
+
+double[] variables = {
+    InputNumber("Введите число k1: "),
+    InputNumber("Введите число b1: "),
+    InputNumber("Введите число k2: "),
+    InputNumber("Введите число b2: ") };
+
+if (variables[0] == variables[2] && variables[1] == variables[3])
+{
+    Console.WriteLine("Прямые совпадают!");
+}
+else if (variables[0] == variables[2])
+{
+    Console.WriteLine("Прямые параллельны!");
+}
+else
+{
+    double[] point = IntersectionPointOfLines(variables);
+
+    Console.WriteLine($"Точка пересечения двух прямых: ({point[0]} ; {point[1]})");
+}
+
+#region Methods
+double InputNumber(string message)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool result = double.TryParse(Console.ReadLine(), out double number);
+
+        if (!result)
+        {
+            Console.WriteLine($"Введены некоректные данные. {message} еще раз!");
+            Thread.Sleep(1500);
+
+            continue;
+        }
+
+        return number;
+    }
+}
+
+double[] IntersectionPointOfLines(double[] variables)
+{
+    double[] point = new double[2];
+
+    point[0] = (variables[3] - variables[1]) / (variables[0] - variables[2]);
+    point[1] = variables[0] * point[0] + variables[1];
+
+    return point;
+}
+
+#endregion
